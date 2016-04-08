c36da4c009ea2acf1b57d9dea811c80d46401663
tamaracarlos <tamaraselau@hotmail.com>
2016-04-08 11:36:36 -0300

    Criado rodapé para ser padrão em todas as páginas;

---
+++ html/rodape.php
@@ -0,0 +1,10 @@
+<!-- Rodapé -->
+<footer class="footer">
+  <nav class="navbar navbar-default">
+    <div class="container-fluid">
+       <p class="navbar-text"> Desenvolvido em 2016 por Tamara Carlos. </p>
+     </div>
+   </nav>
+</footer>
+</body>
+</html>
